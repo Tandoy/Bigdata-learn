15a09ac5a0e1a9532c8c935002f3ad88ac262aac
tang-engineer <tangzhi1318@163.com>
2020-09-26 17:53:00 +0800

    Spark：共享变量

---
+++ Spark/Spark入门到精通/Core/RDD/共享变量/Scala/AccumulatorVariable$.java
@@ -0,0 +1,21 @@
+/**
+ * @author Administrator
+ */
+object AccumulatorVariable{
+
+        def main(args:Array[String]){
+        val conf=new SparkConf()
+        .setAppName("AccumulatorVariable")
+        .setMaster("local")
+        val sc=new SparkContext(conf)
+
+        val sum=sc.accumulator(0)
+
+        val numberArray=Array(1,2,3,4,5)
+        val numbers=sc.parallelize(numberArray,1)
+        numbers.foreach{num=>sum+=num}
+
+        println(sum)
+        }
+
+        }
---
+++ Spark/Spark入门到精通/Core/RDD/共享变量/Scala/BroadcastVariable$.java
@@ -0,0 +1,22 @@
+/**
+ * @author Administrator
+ */
+object BroadcastVariable{
+
+        def main(args:Array[String]){
+        val conf=new SparkConf()
+        .setAppName("BroadcastVariable")
+        .setMaster("local")
+        val sc=new SparkContext(conf)
+
+        val factor=3;
+        val factorBroadcast=sc.broadcast(factor)
+
+        val numberArray=Array(1,2,3,4,5)
+        val numbers=sc.parallelize(numberArray,1)
+        val multipleNumbers=numbers.map{num=>num*factorBroadcast.value}
+
+        multipleNumbers.foreach{num=>println(num)}
+        }
+
+        }
