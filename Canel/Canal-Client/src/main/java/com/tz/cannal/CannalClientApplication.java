e86d7216aaffd11834ee6073b0ee2f772f2e07db
Tandoy <tangzhi1318@163.com>
2021-03-17 16:28:16 +0800

    Canel：java-client消费mysql binlog

---
+++ Canel/Canal-Client/src/main/java/com/tz/cannal/CannalClientApplication.java
@@ -0,0 +1,13 @@
+package com.tz.cannal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CannalClientApplication.class, args);
+	}
+
+}
---
+++ Canel/Canal-Client/src/test/java/com/tz/cannal/CannalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tz.cannal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannalClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
