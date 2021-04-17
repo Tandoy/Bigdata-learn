041bedf273c5df3399a70e0cea2b7c4c475bc150
Tandoy <tangzhi1318@163.com>
2021-04-17 11:16:43 +0800

    Flink：Flink-Kafka-Connector topic和partition的动态发现

--- Flink/Flink-1.11.2/src/main/java/com/flink/source/SourceKafka.java
+++ Flink/Flink-1.11.2/src/main/java/com/flink/source/SourceKafka.java
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class SourceKafka {
     public static void main(String[] args) throws Exception {
@@ -13,7 +14,11 @@ public class SourceKafka {
         // kafka 配置项
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "dxbigdata103:9092");
-        DataStreamSource<String> streamSource = env.addSource(new FlinkKafkaConsumer011<String>("GMALL_STARTUP", new SimpleStringSchema(), properties));
+        // topic 和 partition 动态发现
+        // 自动发现消费的partition变化
+        properties.setProperty("flink.partition-discovery.interval-millis", String.valueOf((10 * 1000)));
+        Pattern topicPattern = Pattern.compile("topic[0-9]");
+        DataStreamSource<String> streamSource = env.addSource(new FlinkKafkaConsumer011<String>(topicPattern, new SimpleStringSchema(), properties));
         streamSource.print();
         env.execute("test-kafka");
     }
