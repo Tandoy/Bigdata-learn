b6c7cbf8104e59f87a5730bf145e66eca235c3fd
Tandoy <tangzhi1318@163.com>
2021-05-31 11:48:40 +0800

    Flink-CDC：通过Flink-CDC获取binlog，Temporal Join kafka维度版本表

--- Flink/flink-cdc/src/main/java/com/tz/flink/tableapi/FlinkCDCTemporalJoin.java
+++ Flink/flink-cdc/src/main/java/com/tz/flink/tableapi/FlinkCDCTemporalJoin.java
@@ -6,7 +6,7 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 /**
  * 使用flink sql来实现：
  * 1.flink-cdc捕获mysql upsert 订单表数据
- * 2.kafka-connectors捕获topic 产品维度版本表数据
+ * 2.kafka捕获topic 产品维度版本表数据
  * 3.实现TemporalJoin
  */
 public class FlinkCDCTemporalJoin {
@@ -14,6 +14,7 @@ public class FlinkCDCTemporalJoin {
         // 1.创建流执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
         // 2.source：mysql-cdc 订单表
         tableEnv.executeSql("CREATE TABLE demoOrders (\n" +
                 "         `order_id` INTEGER ,\n" +
@@ -32,7 +33,29 @@ public class FlinkCDCTemporalJoin {
                 "          'database-name' = 'davincidb',\n" +
                 "          'debezium.snapshot.locking.mode' = 'none',\n" +
                 "          'table-name' = 'demo_orders')\n");
-        tableEnv.executeSql("select * from demoOrders").print();
-        // TODO 3.从kafka中获取产品维度版本表数据
+
+        // 3.从kafka中获取产品维度版本表数据
+        tableEnv.executeSql("CREATE TABLE `demoProducts` (\n" +
+                "          `product_id` STRING, \n" +
+                "          `product_name` STRING,\n" +
+                "          `price` DECIMAL(10, 4),\n" +
+                "          `currency` STRING, \n" +
+                "           update_time TIMESTAMP(3) METADATA FROM 'timestamp', \n" +
+                "           PRIMARY KEY(product_id) NOT ENFORCED, \n" +
+                "          WATERMARK FOR update_time AS update_time \n" +
+                "        ) WITH (\n" +
+                "          'connector' = 'kafka',\n" +
+                "          'topic' = 'demo_products',\n" +
+                "          'properties.bootstrap.servers' = 'dxbigdata103:9092',\n" +
+                "          'scan.startup.mode' = 'earliest-offset',\n" +
+                "          'format' = 'debezium-json',\n" +
+                "          'debezium-json.ignore-parse-errors' = 'true')\n");
+        // 4.TemporalJoin
+        // TemporalJoin中version table必须要有主键
+        tableEnv.executeSql("SELECT o.order_id, o.order_date, o.order_time, o.quantity, o.purchaser, p.product_id,\n" +
+                "               p.product_name, p.update_time, p.price, p.currency, p.price * o.quantity as total_price  \n" +
+                "           FROM demoOrders as o \n" +
+                "           LEFT JOIN demoProducts FOR SYSTEM_TIME AS OF o.order_time p \n" +
+                "           ON o.product_id = p.product_id").print();
     }
 }
